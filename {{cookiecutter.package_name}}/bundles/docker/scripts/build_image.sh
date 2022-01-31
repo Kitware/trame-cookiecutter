3b86dcd07438bb7ecda80c1a2c4711903bfccb26
Patrick Avery <patrick.avery@kitware.com>
2022-01-31 14:37:36 -0600

    Update docker bundle for the latest image
    These changes allow the bundle scripts to work with the latest image.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- {{cookiecutter.package_name}}/bundles/docker/scripts/build_image.sh
+++ {{cookiecutter.package_name}}/bundles/docker/scripts/build_image.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 CURRENT_DIR=`dirname "$0"`
 
-cd $CURRENT_DIR
-. build_server.sh
-cd ..
-docker build -t {{cookiecutter.entry_point}}
+. $CURRENT_DIR/build_server.sh
+
+cd $CURRENT_DIR/..
+docker build -t {{cookiecutter.entry_point}} .
+cd -
