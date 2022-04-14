b70a1f76b64995cea0e71535fa5069ba1d213601
Patrick Avery <patrick.avery@kitware.com>
2022-04-14 17:00:45 -0500

    Use engine logger and disable output in jupyter
    This gets the engine to use a logger, and its output is disabled in jupyter.
    This is done so that the output doesn't fill up the screen in a jupyter notebook.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/jupyter.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/jupyter.py
@@ -6,13 +6,27 @@ from trame.jupyter import display_iframe
 from {{cookiecutter.import_name}}.app import controller, ui
 
 
+# Keep track of whether we have ran this function before
+FIRST_RUN = True
+
+
 def run_in_loop(**kwargs):
     """Run and display the trame application in jupyter's event loop
 
     The kwargs are forwarded to IPython.display.IFrame()
     """
-    controller.on_start()
-    ui.layout.start(exec_mode="task", server=True)
+    global FIRST_RUN
+    if FIRST_RUN:
+        # Disable info logging for the engine logger
+        import logging
+        engine_logger = logging.getLogger("{{cookiecutter.import_name}}.app.engine")
+        engine_logger.setLevel(logging.WARNING)
+
+        # Start up trame
+        controller.on_start()
+        ui.layout.start(exec_mode="task", server=True)
+
+        FIRST_RUN = False
 
     def display():
         src = f"http://localhost:{port()}"
