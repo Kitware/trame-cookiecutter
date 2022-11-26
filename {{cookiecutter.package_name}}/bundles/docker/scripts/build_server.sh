1521fdc1257c3661844ac60a929825b7cb8c0530
Patrick Avery <patrick.avery@kitware.com>
2022-11-25 21:30:58 -0500

    chore(docker): hide pushd and popd output
    This just removes some unnecessary output from our terminal.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- {{cookiecutter.package_name}}/bundles/docker/scripts/build_server.sh
+++ {{cookiecutter.package_name}}/bundles/docker/scripts/build_server.sh
@@ -3,12 +3,12 @@ CURRENT_DIR=`dirname "$0"`
 
 # Since Mac doesn't come with realpath by default, let's set the full
 # paths using PWD.
-pushd .
+pushd . > /dev/null
 cd $CURRENT_DIR/..
 DEPLOY_DIR=$PWD
 cd $CURRENT_DIR/../../..
 ROOT_DIR=$PWD
-popd
+popd > /dev/null
 
 docker run --rm          \
     -e TRAME_BUILD_ONLY=1 \
--- {{cookiecutter.package_name}}/bundles/docker/scripts/run_server.sh
+++ {{cookiecutter.package_name}}/bundles/docker/scripts/run_server.sh
@@ -3,10 +3,10 @@ CURRENT_DIR=`dirname "$0"`
 
 # Since Mac doesn't come with realpath by default, let's set the full
 # paths using PWD.
-pushd .
+pushd . > /dev/null
 cd $CURRENT_DIR/..
 DEPLOY_DIR=$PWD
-popd
+popd > /dev/null
 
 docker run -it --rm \
     -p 8080:80 \
