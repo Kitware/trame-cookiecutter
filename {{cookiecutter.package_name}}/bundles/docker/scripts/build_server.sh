6c3239f5f81ef6b8eda504ea095337377e7cd7b9
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-06-05 12:18:27 -0600

    fix(docker): proper path handling

--- {{cookiecutter.package_name}}/bundles/docker/scripts/build_server.sh
+++ {{cookiecutter.package_name}}/bundles/docker/scripts/build_server.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 CURRENT_DIR=`dirname "$0"`
 
-# Since Mac doesn't come with realpath by default, let's set the full
-# paths using PWD.
-pushd . > /dev/null
 cd $CURRENT_DIR/..
-DEPLOY_DIR=$PWD
-cd $CURRENT_DIR/../../..
-ROOT_DIR=$PWD
-popd > /dev/null
+DEPLOY_DIR="$PWD"
+cd ../..
+ROOT_DIR="$PWD"
 
 docker run --rm          \
     -v "$DEPLOY_DIR:/deploy" \
