2bc6baf3074a208514f5dbb7ec99cb8bdbb6e4b9
Ulysse DURAND <ulyssedurand@hotmail.fr>
2026-07-15 12:08:57 +0200

    ci(pre-commit): change files to pass linter and tests

--- {{cookiecutter.package_name}}/examples/docker/README.md
+++ {{cookiecutter.package_name}}/examples/docker/README.md
@@ -1,6 +1,8 @@
 # Docker Bundle
 
-Docker bundle aims to provide an easy step to bundle and deploy your application into a Docker image that can easily be deployed in the cloud as a service and will naturally support multi-users.
+Docker bundle aims to provide an easy step to bundle and deploy your application
+into a Docker image that can easily be deployed in the cloud as a service and
+will naturally support multi-users.
 
 ## Building image
 
--- {{cookiecutter.package_name}}/tests/test_import.py
+++ {{cookiecutter.package_name}}/tests/test_import.py
@@ -1,12 +1,12 @@
 def test_import():
 {%- if cookiecutter.include_app %}
-    from {{ cookiecutter.import_name }}.app import main  # noqa: F401
+    from {{ cookiecutter.import_name }}.app import main
 {%- endif %}
 {%- if cookiecutter.include_components %}
-    from {{ cookiecutter.import_name }}.widgets.{{ cookiecutter.module_short_name }} import CustomWidget  # noqa: F401, PLC0415
+    from {{ cookiecutter.import_name }}.widgets.{{ cookiecutter.module_short_name }} import CustomWidget
 {%- endif %}
 {%- if cookiecutter.include_components_only %}
 
     # For components only, the CustomWidget is also importable via trame
-    from trame.widgets.{{ cookiecutter.import_name }} import CustomWidget  # noqa: F401, PLC0415, F811
+    from trame.widgets.{{ cookiecutter.import_name }} import CustomWidget
 {%- endif %}
